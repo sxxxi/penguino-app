0074434ca4b458860a3fe278906152688e1305c3
Jeciel Benerayan <benerayj@sheridancollege.ca>
2023-04-10 00:02:31 -0400

    Can register names now

--- app/src/main/java/com/penguino/RegisterName.kt
+++ app/src/main/java/com/penguino/RegisterName.kt
@@ -1,15 +1,24 @@
 package com.penguino
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.databinding.DataBindingUtil
 //import androidx.appcompat.app.AppCompatActivity
 //import androidx.databinding.DataBindingUtil
 //import android.widget.TextView
 import androidx.navigation.findNavController
 import com.penguino.databinding.FragmentBluetoothDevicesScanBinding
+import com.penguino.databinding.FragmentRegisterNameBinding
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
+import java.io.IOException
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,7 +34,7 @@ class RegisterName : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var binding: FragmentBluetoothDevicesScanBinding
+    private lateinit var binding: FragmentRegisterNameBinding
 
 
 
@@ -43,13 +52,78 @@ class RegisterName : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-      //  return inflater.inflate(R.layout.fragment_register_name, container, false)
-        binding = FragmentBluetoothDevicesScanBinding.inflate(inflater)
+        binding = FragmentRegisterNameBinding.inflate(inflater, container, false)
 
-        binding.buttonRegisterName.setOnClickListener {view : View ->
-            view.findNavController().navigate(R.id.action_bluetoothDevicesScan_to_fragment_register_name)
+        //initialize http variables
+        val url : String = "http://10.0.2.2:8080/api/pet"
+        val client: OkHttpClient = OkHttpClient()
+
+        //onclick do request
+        binding.registerBtn.setOnClickListener { view: View ->
+
+
+            //Get user inputs
+            val name = binding.addName.text.toString()
+            val personality = binding.addPersonality.text.toString()
+            val age = binding.addAge.text.toString()
+            //make json object
+            val jsonObject = JSONObject()
+            jsonObject.put("name", name)
+            jsonObject.put("personality", personality)
+            jsonObject.put("age", age)
+            val jsonObjectString = jsonObject.toString()
+            val requestBody = jsonObjectString.toRequestBody("application/json".toMediaTypeOrNull())
+            //initialize http requests
+            val request = Request.Builder() //GET
+                .url(url) //'localhost' or ip doesnt work, have to use this because im using emulator
+                .build()
+            val requestPost = Request.Builder() //POST
+                .post(requestBody)
+                .url(url)
+                .build()
+
+            Thread(Runnable {
+                client.newCall(requestPost).enqueue(object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                    override fun onResponse(call: Call, response: Response) {
+                        response.use {
+                            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                            for ((name, value) in response.headers) {
+                                println("$name: $value")
+                            }
+                        }
+                    }
+                })
+
+                client.newCall(request).enqueue(object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                    override fun onResponse(call: Call, response: Response) {
+                        response.use {
+                            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                            for ((name, value) in response.headers) {
+                                println("$name: $value")
+                            }
+                            Log.e("result", response.body!!.string())
+                        }
+                    }
+                })
+            }).start()
         }
+
+
+
         return binding.root
+        // inflater.inflate(R.layout.fragment_register_name, container, false)
+
+
     }
 
     companion object {

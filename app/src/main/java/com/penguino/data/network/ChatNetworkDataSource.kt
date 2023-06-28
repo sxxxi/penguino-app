c5a165322712d17ba1b1351cc5c7cb1c065a5ced
domino <akakabeseiji0@gmail.com>
2023-06-28 17:37:12 -0400

    remove api key

--- app/src/main/java/com/penguino/data/network/ChatNetworkDataSource.kt
+++ app/src/main/java/com/penguino/data/network/ChatNetworkDataSource.kt
@@ -9,7 +9,7 @@ import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
-private const val API_KEY = "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"
+private const val API_KEY = ""
 
 interface ChatNetworkDataSource {
 	@GET("/v1/models")

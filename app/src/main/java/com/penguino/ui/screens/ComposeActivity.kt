8cb68b0c03e05db8526816636b80a0b84cfe92f2
domino <akakabeseiji0@gmail.com>
2023-08-18 13:54:46 -0400

    Remove app button nav

--- app/src/main/java/com/penguino/ui/screens/ComposeActivity.kt
+++ app/src/main/java/com/penguino/ui/screens/ComposeActivity.kt
@@ -43,105 +43,69 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ComposeActivity : ComponentActivity() {
-    // A popup for granting access. This is temporary and I am looking for better alternatives.
-    private val multiplePermissionLauncher: ActivityResultLauncher<Array<String>> =
-        registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) { }
+	// A popup for granting access. This is temporary and I am looking for better alternatives.
+	private val multiplePermissionLauncher: ActivityResultLauncher<Array<String>> =
+		registerForActivityResult(
+			ActivityResultContracts.RequestMultiplePermissions()
+		) { }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            PenguinoTheme {
-                MainScreen(Modifier.fillMaxSize())
-            }
-        }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			PenguinoTheme {
+				MainScreen(Modifier.fillMaxSize())
+			}
+		}
 
-        // Ask permission on application launch
-        multiplePermissionLauncher.launch(arrayOf(
-            Manifest.permission.BLUETOOTH_CONNECT,
-            Manifest.permission.BLUETOOTH_SCAN,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.POST_NOTIFICATIONS
-        ))
-    }
+		// Ask permission on application launch
+		multiplePermissionLauncher.launch(
+			arrayOf(
+				Manifest.permission.BLUETOOTH_CONNECT,
+				Manifest.permission.BLUETOOTH_SCAN,
+				Manifest.permission.ACCESS_FINE_LOCATION,
+				Manifest.permission.ACCESS_COARSE_LOCATION,
+				Manifest.permission.POST_NOTIFICATIONS
+			)
+		)
+	}
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun MainScreen(
-    modifier: Modifier = Modifier,
-    navController: NavHostController = rememberNavController()
+	modifier: Modifier = Modifier,
+	navController: NavHostController = rememberNavController()
 ) {
-    data class BottomNavButton(
-        val screen: Screen,
-        val icon: ImageVector,
-        val label: String?
-    )
-
-    val bottomNavButtons = listOf(
-        BottomNavButton(
-            screen = Screen.HomeScreen,
-            icon = Icons.Default.List,
-            label = "Saved"
-        ),
-        BottomNavButton(
-            screen = Screen.ScanScreen,
-            icon = Icons.Default.Add,
-            label = "New"
-        )
-    )
-
-    Scaffold(
-        bottomBar = {
-            NavigationBar {
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentDestination = navBackStackEntry?.destination
-
-                bottomNavButtons.forEach { button ->
-                    NavigationBarItem(
-                        icon = { Icon(imageVector = button.icon, null) },
-                        label = {
-                            if (button.label != null) {
-                                Text(text = button.label)
-                            }
-                        },
-                        selected = currentDestination?.hierarchy?.any { it.route == button.screen.route } == true,
-                        onClick = {
-                            navController.navigate(button.screen.route) {
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    inclusive = true
-                                    saveState = true
-                                }
-                                // Avoid multiple copies of the same destination when
-                                // reselecting the same item
-                                launchSingleTop = true
-                                // Restore state when reselecting a previously selected item
-                                restoreState = false
-                            }
-                        }
-                    )
-                }
-            }
-        }
-    ) {
-        NavHost(
-            navController = navController,
-            startDestination = Screen.HomeScreen.route,
-            modifier = modifier.padding(it)
-        ) {
-            homeScreen(onSavedPetClicked = navController::navigateToPetInfo)
-            scanScreen(onNavigateToRegistration = navController::navigateToRegistration)
-            remoteControlScreen()
-            petInfo(
-                onNavigateToHome = navController::navigateToHome,
-                onNavigateToRc = navController::navigateToRemoteControl,
-                onBackPressed = navController::popBackStack
-            )
-            registrationScreen(
-                onNavigateToHome = navController::navigateToHome
-            )
-        }
-    }
+	val screenTransitionDuration = remember { 500 }
+	Scaffold {
+		NavHost(
+			navController = navController,
+			startDestination = Screen.HomeScreen.route,
+			modifier = modifier.padding(it),
+			enterTransition = {
+				slideInHorizontally(tween(screenTransitionDuration)) +
+						fadeIn(tween(screenTransitionDuration))
+			},
+			exitTransition = {
+				slideOutHorizontally(tween(screenTransitionDuration)) +
+						fadeOut(tween(screenTransitionDuration))
+			}
+		) {
+			homeScreen(
+				onSavedPetClicked = navController::navigateToPetInfo,
+				onPetAdd = navController::navigateToScan
+			)
+			scanScreen(onNavigateToRegistration = navController::navigateToRegistration)
+			remoteControlScreen()
+			petInfo(
+				onNavigateToHome = navController::navigateToHome,
+				onNavigateToRc = navController::navigateToRemoteControl,
+				onBackPressed = navController::popBackStack
+			)
+			registrationScreen(
+				onNavigateToHome = navController::navigateToHome,
+				onBackPressed = navController::popBackStack
+			)
+		}
+	}
 }

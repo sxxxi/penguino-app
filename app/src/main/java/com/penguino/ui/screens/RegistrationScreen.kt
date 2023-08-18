3e9b9914616512cbcf7a3ae20e55a6d0c90c7878
domino <akakabeseiji0@gmail.com>
2023-08-18 13:58:03 -0400

    Update registration ui to be more interactive

--- app/src/main/java/com/penguino/ui/screens/RegistrationScreen.kt
+++ app/src/main/java/com/penguino/ui/screens/RegistrationScreen.kt
@@ -1,104 +1,280 @@
 package com.penguino.ui.screens
 
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.penguino.data.local.models.RegistrationInfoEntity
-import com.penguino.ui.components.SimpleTopBar
 import com.penguino.ui.components.TextInput
-import com.penguino.ui.components.TextInputWithSuggestion
 import com.penguino.ui.theme.PenguinoTheme
 import com.penguino.ui.viewmodels.RegistrationViewModel.RegistrationUiState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun RegistrationScreen(
-	modifier: Modifier = Modifier,
 	uiState: RegistrationUiState = RegistrationUiState(regInfo = RegistrationInfoEntity()),
 	onInputChange: ((RegistrationInfoEntity) -> RegistrationInfoEntity) -> Unit = {},
 	onRegInfoPost: () -> Unit = {},
 	onNavigateToHome: () -> Unit = {},
-	onBack: () -> Unit = {}
+	onBack: () -> Unit = {},
 ) {
-	Column(
-		modifier = modifier.fillMaxSize(),
-		horizontalAlignment = Alignment.CenterHorizontally
+	ScreenHolder(
+		screens = listOf(
+			{
+				NamePrompt(
+					name = uiState.regInfo.petName,
+					onInputChange = { newName ->
+						onInputChange { state ->
+							state.copy(petName = newName)
+						}
+					}
+				)
+			},
+			{
+				ImagePrompt()
+			},
+		),
+		loadingScreen = {
+			Text(text = "Loading")
+		},
+		confirmationScreen = {
+			ConfirmationScreen()
+		},
+		onNavigateToHome = onNavigateToHome,
+		onNavigateToPrevious = onBack,
+		onSubmit = onRegInfoPost,
+	)
+}
+
+@Composable
+private fun ProgressIndicator(
+	modifier: Modifier = Modifier,
+	containerColor: Color = MaterialTheme.colorScheme.surfaceVariant,
+	indicatorColor: Color = MaterialTheme.colorScheme.primary,
+	progress: Float = 0.5f
+) {
+	val barHeight = remember { 8.dp }
+	Box(
+		modifier = Modifier
+			.fillMaxWidth()
+			.then(modifier)
+			.clip(RoundedCornerShape(100))
 	) {
-		SimpleTopBar(title = "Enter info", onNavBack = onBack)
-		TextFields(
-			modifier = modifier
-				.align(Alignment.CenterHorizontally),
-			suggestions = uiState.suggestions,
-			regInfo = uiState.regInfo,
-			regInfoUpdate = onInputChange,
-			onPostRequest = {
-				onRegInfoPost()
-				onNavigateToHome()
+		Box(modifier = Modifier
+			.height(barHeight)
+			.background(containerColor)
+			.fillMaxWidth()
+			.drawBehind {
+				drawRoundRect(
+					color = indicatorColor,
+					cornerRadius = CornerRadius(100f, 100f),
+					size = this.size.copy(width = size.width * progress)
+				)
 			}
 		)
 	}
 }
 
 @Composable
-private fun TextFields(
-	modifier: Modifier = Modifier,
-	suggestions: List<String>,
-	regInfo: RegistrationInfoEntity,
-	regInfoUpdate: ((RegistrationInfoEntity) -> RegistrationInfoEntity) -> Unit,
-	onPostRequest: () -> Unit = {}
+private fun ScreenHolder(
+	screens: List<@Composable ColumnScope.() -> Unit>,
+	loadingScreen: @Composable ColumnScope.() -> Unit = {},
+	confirmationScreen: @Composable ColumnScope.() -> Unit = {},
+	coroutineScope: CoroutineScope = rememberCoroutineScope(),
+	onSubmit: () -> Unit = {},
+	onNavigateToPrevious: () -> Unit,
+	onNavigateToHome: () -> Unit,
 ) {
-	Column(
-		modifier = modifier.fillMaxSize(),
-		horizontalAlignment = Alignment.CenterHorizontally,
-		verticalArrangement = Arrangement.Center
-	) {
-		TextInputWithSuggestion(
-			value = regInfo.petName,
-			suggestions = suggestions,
-			onValueChange = { name ->
-				regInfoUpdate { regInfo ->
-					regInfo.copy(petName = name)
+	if (screens.isNotEmpty()) {
+		var isSubmitted by remember { mutableStateOf(false) }
+		var isLoading by remember { mutableStateOf(false) }
+		var currentIndex by remember { mutableIntStateOf(0) }
+		val progress: Float by animateFloatAsState(
+			targetValue = ((currentIndex + 1).toFloat() / screens.size.toFloat()),
+			label = "Progress animation",
+			animationSpec = spring(
+				dampingRatio = Spring.DampingRatioLowBouncy,
+				stiffness = Spring.StiffnessLow
+			)
+		)
+		val currentScreen = remember(currentIndex, isSubmitted) {
+			if (isSubmitted) {
+				if (isLoading) {
+					loadingScreen
+				} else {
+					confirmationScreen
 				}
+			} else {
+				screens[currentIndex]
 			}
-		) { Text(text = "Name") }
+		}
 
-		TextInput(
-			value = regInfo.personality,
-			onValueChange = { newVal ->
-				regInfoUpdate { regInfo ->
-					regInfo.copy(personality = newVal)
-				}
+		BackHandler(enabled = !isSubmitted) {
+			if (currentIndex == 0 || (isSubmitted && !isLoading)) {
+				onNavigateToPrevious()
+			} else {
+				currentIndex -= 1
 			}
-		) { Text(text = "Personality") }
+		}
 
-		TextInput(
-			value = regInfo.age.toString(),
-			keyboardType = KeyboardType.Number,
-			onValueChange = { newInt ->
-				regInfoUpdate { regInfo ->
-					newInt.toIntOrNull()?.let {
-						regInfo.copy(age = it)
-					} ?: regInfo
-				}
-			}
-		) { Text(text = "Age") }
-
-		Button(
-			modifier = modifier.padding(vertical = 8.dp, horizontal = 16.dp),
-			onClick = onPostRequest
+		Column(
+			modifier = Modifier.fillMaxSize(),
+			horizontalAlignment = Alignment.CenterHorizontally
 		) {
-			Text(text = "Register")
+			if (!isSubmitted) {
+				ProgressIndicator(
+					modifier = Modifier.padding(16.dp),
+					progress = progress
+				)
+			}
+			Column(
+				Modifier.weight(1f)
+			) {
+				AnimatedContent(
+					targetState = currentScreen,
+					label = "Current screen",
+					transitionSpec = { (fadeIn() + slideInHorizontally()) togetherWith (fadeOut() + slideOutHorizontally()) }
+				) { screen ->
+					Column(
+						modifier = Modifier
+							.fillMaxSize()
+							.padding(horizontal = 16.dp),
+						verticalArrangement = Arrangement.Center,
+						horizontalAlignment = Alignment.CenterHorizontally
+					) {
+						screen()
+					}
+				}
+			}
+			Row(
+				modifier = Modifier
+					.padding(horizontal = 16.dp)
+					.fillMaxWidth(),
+				horizontalArrangement = Arrangement.SpaceBetween
+			) {
+				if (!isSubmitted) {
+					TextButton(onClick = {
+						if (currentIndex == 0) {
+							onNavigateToPrevious()
+						} else {
+							currentIndex -= 1
+						}
+					}) {
+						Text(text = "Back")
+					}
+
+					Spacer(Modifier.weight(1f))
+
+					if (progress == 1f) {
+						TextButton(onClick = {
+							coroutineScope.launch {
+								isLoading = true
+								isSubmitted = true
+								onSubmit()
+							}.invokeOnCompletion {
+								isLoading = false
+							}
+						}) {
+							Text(text = "Submit")
+						}
+					} else {
+						TextButton(onClick = {
+							currentIndex += 1
+						}) {
+							Text(text = "Next")
+						}
+					}
+				} else {
+					Spacer(Modifier.weight(1f))
+					TextButton(onClick = {
+						onNavigateToPrevious()
+						onNavigateToHome()
+					}) {
+						Text(text = "Done")
+					}
+				}
+			}
 		}
 	}
+}
+
+@Composable
+private fun ColumnScope.NamePrompt(
+	name: String,
+	onInputChange: (String) -> Unit
+) {
+	Spacer(modifier = Modifier.weight(0.5f))
+	Column(
+		Modifier
+			.weight(0.5f)
+	) {
+		Text(
+			text = "What is your new \nfriend's name?",
+			style = MaterialTheme.typography.displaySmall + TextStyle(fontWeight = FontWeight.Bold)
+		)
+		Spacer(modifier = Modifier.height(8.dp))
+		TextInput(
+			value = name,
+			label = null,
+			onValueChange = onInputChange
+		)
+	}
+}
+
+@Composable
+private fun ColumnScope.ImagePrompt() {
+	Text(
+		text = "Do you want to take your friend's picture?",
+		style = MaterialTheme.typography.displaySmall + TextStyle(fontWeight = FontWeight.Bold)
+	)
+}
+
+@Composable
+private fun ColumnScope.ConfirmationScreen() {
+	Text(
+		text = "You're all set!",
+		style = MaterialTheme.typography.displaySmall + TextStyle(fontWeight = FontWeight.Bold)
+	)
 }
 
 @Preview

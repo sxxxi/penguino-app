040695d45aea79492c4b5d9f1e78aa145f88c887
domino <akakabeseiji0@gmail.com>
2023-08-19 19:09:14 -0400

    update registration Screen

--- app/src/main/java/com/penguino/ui/screens/RegistrationScreen.kt
+++ app/src/main/java/com/penguino/ui/screens/RegistrationScreen.kt
@@ -1,6 +1,14 @@
 package com.penguino.ui.screens
 
+import android.Manifest
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.net.Uri
 import androidx.activity.compose.BackHandler
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
@@ -10,7 +18,9 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,12 +31,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -39,25 +53,35 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.FileProvider
+import androidx.core.net.toUri
+import androidx.exifinterface.media.ExifInterface
 import com.penguino.data.local.models.RegistrationInfoEntity
 import com.penguino.ui.components.TextInput
 import com.penguino.ui.theme.PenguinoTheme
 import com.penguino.ui.viewmodels.RegistrationViewModel.RegistrationUiState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import java.io.File
+
 
 @Composable
 fun RegistrationScreen(
 	uiState: RegistrationUiState = RegistrationUiState(regInfo = RegistrationInfoEntity()),
 	onInputChange: ((RegistrationInfoEntity) -> RegistrationInfoEntity) -> Unit = {},
-	onRegInfoPost: () -> Unit = {},
+	onPfpChange: (Bitmap?) -> Unit = {},
+	onRegInfoPost: (Context) -> Unit = {},
 	onNavigateToHome: () -> Unit = {},
 	onBack: () -> Unit = {},
 ) {
+	val context = LocalContext.current
 	ScreenHolder(
 		screens = listOf(
 			{
@@ -71,7 +95,10 @@ fun RegistrationScreen(
 				)
 			},
 			{
-				ImagePrompt()
+				ImagePrompt(
+					pfp = uiState.pfp,
+					onPfpChange = onPfpChange
+				)
 			},
 		),
 		loadingScreen = {
@@ -82,7 +109,7 @@ fun RegistrationScreen(
 		},
 		onNavigateToHome = onNavigateToHome,
 		onNavigateToPrevious = onBack,
-		onSubmit = onRegInfoPost,
+		onSubmit = { onRegInfoPost(context) },
 	)
 }
 
@@ -262,9 +289,117 @@ private fun ColumnScope.NamePrompt(
 }
 
 @Composable
-private fun ColumnScope.ImagePrompt() {
+fun ColumnScope.ImagePrompt(
+	pfp: Bitmap? = null,
+	onPfpChange: (Bitmap?) -> Unit = {}
+) {
+	// Get and store cache photo -> registerForResult
+	// Compress bitmap to Jpeg
+	// Store in specific directory
+	var image by remember { mutableStateOf<Bitmap?>(null) }
+	val cacheDir = LocalContext.current.cacheDir
+	var tempImageCache by remember { mutableStateOf(Uri.EMPTY) }
+	val context = LocalContext.current
+	var permissionGranted by remember { mutableStateOf(false) }
+
+	var tempFile by remember {
+		mutableStateOf<File?>(null)
+	}
+
+	val launchCamera = rememberLauncherForActivityResult(
+		contract = ActivityResultContracts.TakePicture(),
+		onResult = { success ->
+			if (success) {
+				// Get image orientation from image EXIF
+
+				context.contentResolver.openInputStream(tempFile!!.toUri())?.use { iStream ->
+					val rotate = ExifInterface(iStream).getAttributeInt(
+						ExifInterface.TAG_ORIENTATION,
+						ExifInterface.ORIENTATION_UNDEFINED
+					).let { orientation ->
+						when (orientation) {
+							ExifInterface.ORIENTATION_ROTATE_90 -> 90f
+							ExifInterface.ORIENTATION_ROTATE_180 -> 180f
+							ExifInterface.ORIENTATION_ROTATE_270 -> 270f
+							else -> 0f
+						}
+					}
+
+					// rotate temp file
+					tempFile?.let { file ->
+						val bitmap = BitmapFactory.decodeFile(file.path)
+						val matrix = Matrix()
+						matrix.postRotate(rotate)
+						Bitmap.createBitmap(
+							bitmap,
+							0,
+							0,
+							bitmap.width,
+							bitmap.height,
+							matrix,
+							true
+						)
+					}.let {
+						onPfpChange(it)
+					}
+				}
+			}
+		}
+	)
+
+	val cameraPermission = rememberLauncherForActivityResult(
+		contract = ActivityResultContracts.RequestPermission(),
+		onResult = {
+			permissionGranted = it
+		}
+	)
+
+	LaunchedEffect(key1 = Unit) {
+		cameraPermission.launch(Manifest.permission.CAMERA)
+	}
+
+	Box(
+		modifier = Modifier
+			.clickable {
+				if (permissionGranted) {
+					tempFile = File
+						.createTempFile("pfp_cache", ".jpg", cacheDir)
+						.apply {
+							createNewFile()
+						}
+						.let {
+							tempImageCache = FileProvider.getUriForFile(
+								context, "com.penguino.FileProvider", it
+							)
+							launchCamera.launch(tempImageCache)
+							it
+						}
+				}
+			}
+			.size(78.dp)
+			.clip(RoundedCornerShape(100))
+	) {
+		Column(
+			modifier = Modifier
+				.background(Color.Gray)
+				.fillMaxSize(),
+			horizontalAlignment = Alignment.CenterHorizontally,
+			verticalArrangement = Arrangement.Center
+		) {
+			pfp?.let { bitmap ->
+				Image(
+					modifier = Modifier.fillMaxSize(),
+					bitmap = bitmap.asImageBitmap(),
+					contentDescription = "preview",
+					contentScale = ContentScale.Crop
+				)
+			} ?: Icon(imageVector = Icons.Default.Add, contentDescription = "add")
+		}
+	}
+
+
 	Text(
-		text = "Do you want to take your friend's picture?",
+		text = "Take a photo of your new friend!",
 		style = MaterialTheme.typography.displaySmall + TextStyle(fontWeight = FontWeight.Bold)
 	)
 }
@@ -281,8 +416,15 @@ private fun ColumnScope.ConfirmationScreen() {
 @Composable
 fun PreviewRegistrationScreen() {
 	PenguinoTheme {
-		Surface {
-			RegistrationScreen()
+		Column(
+			Modifier
+				.fillMaxSize()
+				.background(MaterialTheme.colorScheme.background),
+			horizontalAlignment = Alignment.CenterHorizontally,
+			verticalArrangement = Arrangement.Center
+		) {
+			ImagePrompt(onPfpChange = {})
+//			RegistrationScreen()
 		}
 	}
 }
